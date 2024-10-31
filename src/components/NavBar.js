457200c82be8d3c4015bb81fa1bd1cd5872aadfb
Ian Kabaka <iankabaka1@gmail.com>
2024-10-31 23:32:15 +0300

    solved the href tricky link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const newhref = ["#home", "#about", "#projects"];
+  const newArray = links.map((newlink, index) => {
+    return (
+      <a key={newlink} href={newhref[index]}>
+        {newlink}
+      </a>
+    );
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{newArray}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const elemTech = technologies.map((element, index) => {
+    return <span key={index}>{element}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{elemTech}</div>
     </div>
   );
 }
